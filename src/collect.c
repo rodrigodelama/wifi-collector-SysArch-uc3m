e4b1ced3e5ca495024c1a503e4230067d61c574a
Rodrigo De Lama <rodrigo.delama@icloud.com>
2021-10-04 22:42:49 +0200

    style edits: edited collect.c styling

--- src/collect.c
+++ src/collect.c
@@ -2,20 +2,22 @@
 //Declaration of our librarys
 #include "../incl/cells.h"
 
-int ask_cell_num(){
+int ask_cell_num()
+{
     int num;
-    do{
-      printf("What cell do you want to collect? (1 - 21): \n");
-      scanf("%d", &num); 
+    do
+    {
+        printf("What cell do you want to collect? (1 - 21): \n");
+        scanf("%d", &num); 
         
-        if(num<1||num>21)
+        if (num < 1 || num > 21)
             printf("ERROR--Wrong cell option\n"); 
 
-    }while (num<1||num>21);
+    } while (num < 1 || num > 21);
     
  return num;
     
 }
-cell_st.cell_X = ask_cell_num();
+//cell_st.cell_X = ask_cell_num();
 
 
