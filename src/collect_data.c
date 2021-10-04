34e2e90036f8b5024611485d2e2c6f6236b846e9
Gorka Bernad <gbernadsantos@gmail.com>
2021-10-04 21:38:57 +0200

    test

---
+++ src/collect_data.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+//Declaration of our librarys
+#include "../incl/cells.h"
+
+int ask_cell_num(cell_st cell){
+printf("What cell do you want to collect? (1 - 21): ");
+scanf("%s" );    
+
+}
+
+
