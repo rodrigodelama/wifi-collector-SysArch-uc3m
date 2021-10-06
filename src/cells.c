e0aa5ff78f2703be74c4b48ef0a46837c909257a
Gorka Bernad <gbernadsantos@gmail.com>
2021-10-06 21:50:36 +0200

    ajustes al draft

--- src/cells.c
+++ src/cells.c
@@ -4,10 +4,12 @@ void print_all();
 //boolean function to check to validate the MCA input(Draft)
 //!!!!!!PROBLEM with Mac_input(line 8 and 15) (to review)   
 int MAC_check(char Mac_input[16]){
+    int outcome = 1
 
     if((!Mac_input[2].equals('-')&&!Mac_input[5].equals('-')&&!Mac_input[8].equals('-')&&!Mac_input[11].equals('-')&&!Mac_input[14].equals('-'))||(!Mac_input[2].equals(':')&&!Mac_input[5].equals(':')&&!Mac_input[11].equals(':')&&!Mac_input[14].equals(':')))
     {
-        return 1;
+       outcome = 0; 
+       return outcome;
     }
 
     int j=0;
@@ -24,9 +26,9 @@ int MAC_check(char Mac_input[16]){
         }
 
         if(j==11)
-            return 1;
-
+            outcome = 0;
+            
     }
-   
+   return outcome;
 
 }
