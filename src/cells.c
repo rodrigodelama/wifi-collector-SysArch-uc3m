2e565b522ad9e5a005c152008129a42547af449e
Gorka Bernad <gbernadsantos@gmail.com>
2021-10-06 21:53:09 +0200

    small fix of earlier parch

--- src/cells.c
+++ src/cells.c
@@ -4,7 +4,8 @@ void print_all();
 //boolean function to check to validate the MCA input(Draft)
 //!!!!!!PROBLEM with Mac_input(line 8 and 15) (to review)   
 int MAC_check(char Mac_input[16]){
-    int outcome = 1
+    
+    int outcome = 1;
 
     if((!Mac_input[2].equals('-')&&!Mac_input[5].equals('-')&&!Mac_input[8].equals('-')&&!Mac_input[11].equals('-')&&!Mac_input[14].equals('-'))||(!Mac_input[2].equals(':')&&!Mac_input[5].equals(':')&&!Mac_input[11].equals(':')&&!Mac_input[14].equals(':')))
     {
