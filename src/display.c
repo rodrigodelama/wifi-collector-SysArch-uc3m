e8d1c8521d0656cd8050b1833a01500885b70ec6
Gorka Bernad <gbernadsantos@gmail.com>
2021-10-07 15:33:12 +0200

    indv Display final draft

--- src/display.c
+++ src/display.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include<string.h>
 #include"../incl/cells.h"
 
 // Display option meant to print data from the requested, or all cells onto the commandline
 
 // TODO: create display individual Cells or the complete array
-void display_cell()
+void display_ind_cell()
 {
     
    int num_cell_to_display = ask_cell_num(1, 21, " "); //registration of the cell to display
@@ -15,10 +16,26 @@ void display_cell()
       {
          printf("Cell %d:",cell_storage[i].cell_X, " %s", cell_storage[i].MAC_Address, " %s", cell_storage[i].ESSID, " %s", cell_storage[i].Mode, " %d", cell_storage[i].channel, "%s", cell_storage[i].encryption, " %s", cell_storage[i].quality, "%s", cell_storage[i].frequency, "%s\n", cell_storage[i].signal_lvl);
       }
+
+      if(strlen(cell_storage[i].cell_X)==0)
+      break;
    }
 
    printf("Do you want to add another access point? [y/N]: ");
       char result = getchar();
+      switch (result)
+      {
+      case 'y':
+      case 'Y': 
+
+         printf("\nIndicate the number of the cell for which you want to know its information (1 - 21):\n");
+         display_cell();
+         break;
       
+      case 'n':
+      case 'N':
+       //action to make if chosen NO
+         break;
+      }
 
 }   
