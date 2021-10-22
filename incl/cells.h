fc776dfc6852b9b85d9ddf2669b0ec975fcb66da
Gorka Bernad <gbernadsantos@gmail.com>
2021-10-22 17:28:17 +0200

    small early adjustments

--- incl/cells.h
+++ incl/cells.h
@@ -3,7 +3,7 @@
 
 #ifndef CELLS_H
 #define CELLS_H
-
+#include "../incl/sizes.h"
 #define CELL_SIZE 80 //Fixed array size
 
 struct cell_st
@@ -17,58 +17,17 @@ struct cell_st
     char quality[CELL_SIZE];
     char frequency[CELL_SIZE];
     char signal_lvl[CELL_SIZE];
+    char Address[CELL_SIZE];
     
 };
 
-struct cell_st info_cell_01[CELL_SIZE];
-struct cell_st info_cell_02[CELL_SIZE];
-struct cell_st info_cell_03[CELL_SIZE];
-struct cell_st info_cell_04[CELL_SIZE];
-struct cell_st info_cell_05[CELL_SIZE];
-struct cell_st info_cell_06[CELL_SIZE];
-struct cell_st info_cell_07[CELL_SIZE];
-struct cell_st info_cell_08[CELL_SIZE];
-struct cell_st info_cell_09[CELL_SIZE];
-struct cell_st info_cell_10[CELL_SIZE];
-struct cell_st info_cell_11[CELL_SIZE];
-struct cell_st info_cell_12[CELL_SIZE];
-struct cell_st info_cell_13[CELL_SIZE];
-struct cell_st info_cell_14[CELL_SIZE];
-struct cell_st info_cell_15[CELL_SIZE];
-struct cell_st info_cell_16[CELL_SIZE];
-struct cell_st info_cell_17[CELL_SIZE];
-struct cell_st info_cell_18[CELL_SIZE];
-struct cell_st info_cell_19[CELL_SIZE];
-struct cell_st info_cell_20[CELL_SIZE];
-struct cell_st info_cell_21[CELL_SIZE];
+struct cell_st cells[LINE_SIZE];
 
-/*
-
-int Index_1 = 0;
-int Index_2 = 0;
-int Index_3 = 0;
-int Index_4 = 0;
-int Index_5 = 0;
-int Index_6 = 0;
-int Index_7 = 0;
-int Index_8 = 0;
-int Index_9 = 0;
-int Index_10 = 0;
-int Index_11 = 0;
-int Index_12 = 0;
-int Index_13 = 0;
-int Index_14 = 0;
-int Index_15 = 0;
-int Index_16 = 0;
-int Index_17 = 0;
-int Index_18 = 0;
-int Index_19 = 0;
-int Index_20 = 0;
-int Index_21 = 0;
-
-*/
-
-void print_X();
-void print_all();
+void insert_new_cell(int cell_n, char MAC_Address[LINE_SIZE], char ESSID[LINE_SIZE], 
+                     char mode[LINE_SIZE], int channel, char encryption[LINE_SIZE],
+                     char quality[LINE_SIZE], char frequency[LINE_SIZE], char signal_lvl[LINE_SIZE]);
+void display_ind_cell();
+void display_all();
+int find_cell(char cell_n);
 
 #endif
---
+++ incl/sizes.h
@@ -0,0 +1,13 @@
+//Rodrigo De Lama - 100451775@alumnos.uc3m.es
+//Gorka Bernad - 100451457@alumnos.uc3m.es
+
+#ifndef SIZES_H
+#define SIZES_H
+
+#define MAX_STRING_SIZE 80
+
+#define ARRAY_SIZE 200
+
+#define LINE_SIZE 80
+
+#endif
