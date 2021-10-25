a85a560f53ebe6359b6eb9156943b8d192fbc9e2
rodrigodelama <rodrigo.delama@icloud.com>
2021-10-25 16:39:02 +0200

    merge: testing ideas in lines 28 and 32 to grab actual data

--- src/cells.c
+++ src/cells.c
@@ -25,11 +25,11 @@ void insert_new_cell(int cell_n, char MAC_Address[LINE_SIZE], char ESSID[LINE_SI
 
     //FIXME: Data is not being added correctly to the dynamic array
     //Add data to the structs
-    //ncell_n = (intptr_t) &cells[c_index].cell_n; //casting - https://stackoverflow.com/questions/22624737/casting-a-pointer-to-an-int
+    &cells[c_index].cell_n = cell_n;
     strcpy(cells[c_index].MAC_Address, MAC_Address);
     strcpy(cells[c_index].ESSID, ESSID);
     strcpy(cells[c_index].mode, mode);
-    channel = (intptr_t) &cells[c_index].channel;
+    &cells[c_index].channel = channel;
     strcpy(cells[c_index].encryption, encryption);
     strcpy(cells[c_index].quality, quality);
     strcpy(cells[c_index].frequency, frequency);
@@ -44,7 +44,7 @@ void insert_new_cell(int cell_n, char MAC_Address[LINE_SIZE], char ESSID[LINE_SI
             &cells[c_index].channel, cells[c_index].encryption,
             cells[c_index].quality, cells[c_index].frequency,
             cells[c_index].signal_lvl);
-  c_index++;
+    c_index++;
 }
 
 // Display option meant to print data from the requested, or all cells onto the commandline
