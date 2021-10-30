9736ed0d70430014c903f68380f954c4e88bd4ff
Gorka Bernad <gbernadsantos@gmail.com>
2021-10-30 12:43:51 +0200

    delete xpansion

--- src/delete_net.c
+++ src/delete_net.c
@@ -10,12 +10,19 @@
 void delete_net(char net[], cell *cells)
 {
    int counter = 0;
-   
+   cell **cell_t_dlt;
+   cell_t_dlt = &cells;
    for(int i = 0; i < c_index; i++)
    {
       if(net == cells[i].ESSID)
       {
-         free(cells[i]);
+         for(int j=i;i < c_index; j++)
+         {
+         *cell_t_dlt = &cells[j+1];
+   
+   
+         }
+         //free(&cell_t_dlt[i]);
       }
    }
 }
